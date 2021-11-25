6d334db9b1e579199acba8d3d64761e67dd191df
Sveta <Sveta@KOMP>
2021-11-25 23:28:54 +0300

    Добавьте файлы проекта.

---
+++ 7.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 3;
+            int y1 = 4;
+            int z1 = 5;
+            int x2 = 6;
+            int y2 = 8;
+            int z2 = 10;
+            double S1 = Ger(x1, y1, z1);
+            double S2 = Ger(x2, y2, z2);
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше площади второго треугольника");
+            }
+            else
+            {
+                Console.WriteLine("Площадь второго треугольника больше площади первого треугольника");
+            }
+            Console.WriteLine("Площадь первого треугольника = {0}, площадь второго треугольника = {1}", S1, S2);
+            Console.ReadKey();
+        }
+        static double Ger(int x, int y, int z)
+        {
+            double p = (x + y + z) / 2;
+            double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return S;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Задание 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Метод вычисления объема и площади поверхности куба.");
+            Console.Write("Введите длину ребра куба: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Cubevol_sq(x);
+            Console.ReadKey();
+        }
+        static void Cubevol_sq(int x)
+        {
+
+            Console.WriteLine("Объем куба равен - " + (x * x * x));
+            Console.WriteLine("Площадь поверхности куба равна - " + (6 * x * x));
+        }
+    }
+}
